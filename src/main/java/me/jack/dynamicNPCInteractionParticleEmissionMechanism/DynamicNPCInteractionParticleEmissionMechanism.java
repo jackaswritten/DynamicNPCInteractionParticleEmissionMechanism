d981c09dbf0e672b74773d1a97571ed41447a167
Jack <96346713+jackaswritten@users.noreply.github.com>
2025-12-28 21:14:42 +0300

    Add files via upload

---
+++ src/main/java/me/jack/dynamicNPCInteractionParticleEmissionMechanism/DynamicNPCInteractionParticleEmissionMechanism.java
@@ -0,0 +1,17 @@
+package me.jack.dynamicNPCInteractionParticleEmissionMechanism;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DynamicNPCInteractionParticleEmissionMechanism extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
